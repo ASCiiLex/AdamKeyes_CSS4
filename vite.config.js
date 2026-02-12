49a0b20ba6ab5da0cf555fc5e865bbc1eefe4d64
ASCiiLex <alejandroscuetara@gmail.com>
2026-02-12 12:37:55 +0100

    Comenzando proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   root: ".",
   server: {
     open: true
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "sass/_settings.scss";`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": "/",
+      "@sass": "/sass",
+      "@js": "/js",
+      "@img": "/img"
+    }
   }
-}
+});
